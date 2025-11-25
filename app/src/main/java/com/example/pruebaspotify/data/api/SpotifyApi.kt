151219323a0fcb7b517fa21d94633e85597cdd7f
Merii <mcabezasbanegas@gmail.com>
2025-11-25 18:22:26 +0100

    Endpoints GET

--- app/src/main/java/com/example/pruebaspotify/data/api/SpotifyApi.kt
+++ app/src/main/java/com/example/pruebaspotify/data/api/SpotifyApi.kt
@@ -1,2 +1,15 @@
-package com.example.pruebaspotify.data.api 
+package com.example.pruebaspotify.data.api
 
+import com.example.pruebaspotify.data.dto.FeaturedPlaylistsDto
+import com.example.pruebaspotify.data.dto.NewReleasesDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface SpotifyApi {
+    @GET("browse/new-releases")
+    suspend fun getNewReleases(@Header("Authorization") token: String): Response<NewReleasesDto>
+
+    @GET("browse/featured-playlists")
+    suspend fun getFeaturedPlaylists(@Header("Authorization") token: String): Response<FeaturedPlaylistsDto>
+}
